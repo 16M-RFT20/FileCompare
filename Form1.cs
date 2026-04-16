5a9e38589b03c395e03e78e881ece5f5e286ef21
kim dg <dgkim729@gmail.com>
2026-04-16 13:03:34 +0900

    과제2 코딩과 테스트 완료 폴더선택기능과파일리스트기능구현(색상구분표시)

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,11 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+
 namespace FileCompare
 {
     public partial class Form1 : Form
@@ -22,6 +30,9 @@ namespace FileCompare
                     var item = new ListViewItem(d.Name);
                     item.SubItems.Add("<DIR>");
                     item.SubItems.Add(d.LastWriteTime.ToString("g"));
+                    // store DirectoryInfo so we can compare later
+                    item.Tag = d;
+                    item.ForeColor = Color.Black;
                     lv.Items.Add(item);
                 }
 
@@ -35,6 +46,9 @@ namespace FileCompare
                     var item = new ListViewItem(f.Name);
                     item.SubItems.Add(f.Length.ToString("N0") + " 바이트");
                     item.SubItems.Add(f.LastWriteTime.ToString("g"));
+                    // store FileInfo so we can compare later
+                    item.Tag = f;
+                    item.ForeColor = Color.Black;
                     lv.Items.Add(item);
                 }
 
@@ -61,6 +75,79 @@ namespace FileCompare
             }
         }
 
+        // Compare two ListView controls and set item colors according to rules:
+        // - identical (same LastWriteTime) -> Black
+        // - different -> newer = Red, older = Gray
+        // - only on one side -> Purple
+        private void CompareAndColor(ListView left, ListView right)
+        {
+            if (left == null || right == null) return;
+
+            var rightMap = new Dictionary<string, ListViewItem>(StringComparer.OrdinalIgnoreCase);
+            foreach (ListViewItem r in right.Items)
+            {
+                rightMap[r.Text] = r;
+                // default color
+                r.ForeColor = Color.Black;
+            }
+
+            var processed = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (ListViewItem l in left.Items)
+            {
+                l.ForeColor = Color.Black;
+                if (rightMap.TryGetValue(l.Text, out var rItem))
+                {
+                    processed.Add(l.Text);
+
+                    var lInfo = l.Tag as FileSystemInfo;
+                    var rInfo = rItem.Tag as FileSystemInfo;
+
+                    if (lInfo != null && rInfo != null)
+                    {
+                        var lTime = lInfo.LastWriteTime;
+                        var rTime = rInfo.LastWriteTime;
+
+                        if (lTime == rTime)
+                        {
+                            l.ForeColor = Color.Black;
+                            rItem.ForeColor = Color.Black;
+                        }
+                        else if (lTime > rTime)
+                        {
+                            l.ForeColor = Color.Red;   // New
+                            rItem.ForeColor = Color.Gray; // Old
+                        }
+                        else
+                        {
+                            l.ForeColor = Color.Gray;
+                            rItem.ForeColor = Color.Red;
+                        }
+                    }
+                    else
+                    {
+                        // If tags are missing, default to black
+                        l.ForeColor = Color.Black;
+                        rItem.ForeColor = Color.Black;
+                    }
+                }
+                else
+                {
+                    // only on left
+                    l.ForeColor = Color.Purple;
+                }
+            }
+
+            // any right-side items not processed are only on right
+            foreach (ListViewItem r in right.Items)
+            {
+                if (!processed.Contains(r.Text))
+                {
+                    r.ForeColor = Color.Purple;
+                }
+            }
+        }
+
         private void btnLeftDir_Click(object sender, EventArgs e)
         {
             using (var dlg = new FolderBrowserDialog())
@@ -78,6 +165,8 @@ namespace FileCompare
                 {
                     txtLeftDir.Text = dlg.SelectedPath;
                     PopulateListView(lvwLeftDir, dlg.SelectedPath);
+                    // update colors comparing both sides
+                    CompareAndColor(lvwLeftDir, lvwrightDir);
                 }
             }
         }
@@ -99,6 +188,7 @@ namespace FileCompare
                 {
                     txtRightDir.Text = dlg.SelectedPath;
                     PopulateListView(lvwrightDir, dlg.SelectedPath);
+                    CompareAndColor(lvwLeftDir, lvwrightDir);
                 }
             }
         }
